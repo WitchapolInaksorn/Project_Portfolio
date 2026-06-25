74a2ed14882f7521dcf2ea16fda3d7f32ca8e71a
OwennXzz <w.inaksorn@gmail.com>
2026-06-26 00:39:30 +0700

    edit name of Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,7 @@
 export default function Footer() {
   return (
     <footer className="footer py-8 text-center text-xs text-slate-600 border-t border-white/5">
-      Built with React + Tailwind · Witchaphon Inaksorn · &copy;{" "}
-      {new Date().getFullYear()}
+      Built with React + Tailwind · Witchaphon Inaksorn
     </footer>
   );
 }
